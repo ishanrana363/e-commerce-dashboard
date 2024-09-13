44193b0bc3e17f5af49244e3993936167123dcad
ishanrana363 <ishanrana363@gmail.com>
2024-09-13 22:15:50 +0600

    file delete

--- src/controllers/passwordForgetController.js
+++
@@ -1,7 +0,0 @@
-const {sendOtoService} = require("../services/forgetPasswordService");
-
-
-exports.sendOtpController = async (req,res)=>{
-    let result = await sendOtoService(req);
-    res.status(201).send(result);
-};
